34228331c73274784b681befac385f0b55a4ea7d
Dylan Parks <dylan@bouwerbytes.com>
2023-11-03 15:53:01 -0500

    Documentation. Began bitboard work.

--- board.py
+++ board.py
@@ -1,7 +1,50 @@
 class Board:
+    """
+    Contains all information on board state
 
-    boardX = 0b0
-    boardY = 0b0
+    ...
 
-    def __init__(self, player1, player2):
+    Attributes
+    -------
+    boardOne: int
+        Binary representation of playerOne's moves. Contains 1's where a move was played and 0's where no move from playerOne was played
+        First 3 bits: Top row
+        Second 3 bits: Middle row
+        Last 3 bits: Bottom row 
+
+
+    boardTwo: int
+    """
+
+    boardOne = 0b0
+    boardTwo = 0b0
+
+    def __init__(self):
+        pass
+
+    def __convertFromBitBoard(self) -> list[list[str]]:
+        """
+        Converts bit board to nested array to make printing easier
+
+        ...
+
+        Returns
+        -------
+        list: list[list[str]]
+        """
+        numBits = 3
+        [print(i) for i in range(numBits - 1, -1, -1)]
+        rowOneBits = [bin((self.boardOne >> bit) & 1) for bit in range(numBits - 1, -1, -1)]
+        print(rowOneBits)
+
+        #boardList = [
+        #    [str(RowOne)],
+        #    [''],
+        #    ['']
+        #]
+        return [['']]
+
+    def printBoard(self):
+        #Board parsing from bitboard
+        self.__convertFromBitBoard()
         pass
--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,42 @@
-import board
+#https://numpydoc.readthedocs.io/en/latest/format.html#documenting-classes
 
-player2Computer = False
+import board
+import player
+
+playerOne = None
+playerTwo = None
+playerTwoComputer = False
+playerOneChar = 'X'
+playerTwoChar = 'O'
+
 
 if __name__ == '__main__':
-    userInput = int(input("Player two computer? 0=false 1=true"))
+    userInput = 0 #!int(input("Player two computer? 0=false 1=true:\n"))
 
     correctInput = False
 
+    # Makes sure userInput is a viable number
     while(correctInput == False):
         if userInput != 0 and userInput != 1:
-            userInput = int(input("Please enter a 0 or a 1. 0=false 1=true"))
+            userInput = int(input("Please enter a 0 or a 1. 0=false 1=true:\n"))
         else:
+            playerTwoComputer = bool(userInput)
             break
+
+    board = board.Board()
+    
+    #? Allow player 1 to be computer?
+    #Sets up player 1 as a normal player, defaults to X as it's char
+    PlayerOne = player.Player(playerOneChar)
+
+    # Sets up player 2 as either a computer or normal player
+    if playerTwoComputer:
+        playerTwo = player.Player(playerTwoChar, playerTwoComputer)
+    else:
+        playerTwo = player.Player(playerTwoChar)
+
+    # Example board for print testing:
+    board.boardOne = 0b001001101
+    board.boardTwo = 0b100010000
+
+    board.printBoard()
